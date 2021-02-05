ddb1803705f93d2d0ac93b8d5ea4bd4da2fdd7a1
Vladis <vladismarkin@gmail.com>
2021-02-05 22:19:33 +0200

    QRadioButton and QButtonGroups

--- 03_simple_qwidgets/radiobutton.py
+++ 03_simple_qwidgets/radiobutton.py
@@ -8,25 +8,50 @@ class DlgMain(QDialog):
         self.setWindowTitle("My GUI")  # add widgets, set properties
 
         self.lbl = QLabel("My label", self)
-        self.lbl.setStyleSheet("color:red")
+        self.lbl.setStyleSheet("color:red; font-size:15px")
         self.lbl.move(50, 30)
+
+        # Color Button Group
+        self.btgColor = QButtonGroup()
 
         self.rbtRed = QRadioButton("red", self)
         self.rbtRed.move(50, 60)
         self.rbtRed.setChecked(True)
         self.rbtRed.clicked.connect(self.evt_rbt_clicked)
+        self.btgColor.addButton(self.rbtRed)
 
         self.rbtBlue = QRadioButton("blue", self)
         self.rbtBlue.move(50, 90)
         self.rbtBlue.clicked.connect(self.evt_rbt_clicked)
+        self.btgColor.addButton(self.rbtBlue)
 
         self.rbtGreen = QRadioButton("green", self)
         self.rbtGreen.move(50, 120)
         self.rbtGreen.clicked.connect(self.evt_rbt_clicked)
+        self.btgColor.addButton(self.rbtGreen)
+
+        # Text size button
+        self.btgSize = QButtonGroup()
+        self.rbtSmall = QRadioButton("Small text", self)
+        self.rbtSmall.move(50,150)
+        self.btgSize.addButton(self.rbtSmall,10)
+        self.rbtRed.clicked.connect(self.evt_rbt_clicked)
+
+        self.rbtMedium = QRadioButton("Medium text", self)
+        self.rbtMedium.setChecked(True)
+        self.rbtMedium.move(50,180)
+        self.btgSize.addButton(self.rbtMedium,15)
+        self.rbtRed.clicked.connect(self.evt_rbt_clicked)
+
+        self.rbtLarge = QRadioButton("Large text", self)
+        self.rbtLarge.move(50,210)
+        self.btgSize.addButton(self.rbtLarge,20)
+        self.rbtRed.clicked.connect(self.evt_rbt_clicked)
 
     def evt_rbt_clicked(self):
-        rbt = self.sender()
-        ss = "color: " + rbt.text()
+        clr = self.btgColor.checkedButton()
+        size = self.btgSize.checkedId()
+        ss = "color:" + clr.text() + "; font-size:" + str(size) + "px"
         self.lbl.setStyleSheet(ss)
 
 
